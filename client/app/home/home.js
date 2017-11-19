7e8ec11e91a2e765bded9a73752228da0b69494e
Peter Symoens <psymoens@eventures.io>
2017-11-19 23:28:58 +0000

    add slider transitions

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -3,36 +3,67 @@
 angular.module('evtrs-site')
   .controller('HomeController', function ($window, $scope, $element, $timeout) {
 
-   $window.tiltFx.init();
+    $window.tiltFx.init();
 
-   var isScrolling =  false;
+    var element = $element[0];
 
-   var wheelEventHandler = function(event){
+    var imgContainer = element.querySelector('.img-container');
+    var contentContainer = element.querySelector('.content-container');
+    var screenHeight = element.offsetHeight;
 
-     if (!isScrolling) {
+    var tll = new TimelineLite();
+    var isScrolling = false;
 
-       $timeout(function() {
+    var transition = function (direction) {
+      var incrementOne;
+      var incrementTwo;
+      if (direction === 'down') {
+        incrementOne = '+=';
+        incrementTwo = '-=';
+      } else if (direction === 'up') {
+        incrementOne = '-=';
+        incrementTwo = '+=';
+      }
+      tll.to(imgContainer, 1.5, {y: incrementTwo.concat(screenHeight), ease: Power3.easeInOut})
+      tll.to(contentContainer, .4, {css: {opacity: 0.4}, ease: Power2.easeIn}, "-=1.5")
+      tll.to(contentContainer, 1.2, {y: incrementOne.concat(screenHeight + 50), ease: Power4.easeIn}, "-=1.5")
+      tll.to(contentContainer, .5, {css: {opacity: 1}, ease: Power2.easeOut}, "-=.5");
 
-         if(event.deltaY > 20){
-           console.log('scrolled up: ' , event);
-         }
+    }
 
-         if(event.deltaY < -20){
-           console.log('scrolled down: ' , event);
-         }
+    var wheelEventHandler = function (event) {
 
-         isScrolling = false;
-       }, 200);
+      if (!isScrolling) {
 
-       isScrolling = true;
+        $timeout(function () {
 
-     };
+          if (event.deltaY > 20) {
+            console.log('scrolled down: ', event);
+            transition('down');
+          }
 
-   }
+          if (event.deltaY < -20) {
+            console.log('scrolled up: ', event);
+            transition('up');
+          }
 
-   var element = angular.element($element)[0];
+          isScrolling = false;
+        }, 200);
 
-   $window.addWheelListener(element, wheelEventHandler);
+        isScrolling = true;
+
+      }
+
+    }
+
+    $scope.navClickHandler = function (direction) {
+        transition(direction);
+
+    }
+
+    var element = angular.element($element)[0];
+
+    $window.addWheelListener(element, wheelEventHandler);
 
 
   });
